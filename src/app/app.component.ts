40e26574a13926284be3ea27da4848827537e95f
Rafi Hadiyasa <rafihadiyasa32@gmail.com>
2024-08-02 23:18:28 +0700

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-project';
+  title: string = 'Bash Application';
+  btn: string = 'Submit Name';
+  angularImage: string = '../assets/angular.png';
+
+  // attribute binding
+  isDisable: boolean = true;
+
+  // event binding
+  toggleButtonDisabled = () => {
+    this.isDisable = !this.isDisable;
+  };
+
+  // two way data binding
+  name: string = '';
 }
